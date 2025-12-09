7bcadb9c02b9cce488cf98f706f06fb6d2c80730
nicocatxzc <nicocatxzc@163.com>
2025-12-09 13:47:49 +0800

    添加分类页面

--- app/components/Page/Taxonomy.vue
+++ app/components/Page/Taxonomy.vue
@@ -1,22 +1,59 @@
-<script lang="ts" setup>
+<script setup>
 const props = defineProps({
     page: {
         type: Object,
         required: true,
     },
 });
+const api = computed(() => {
+    let query = {};
+    if (props.page?.type == "category") {
+        query.categoryId = props.page.id;
+    }
+    if (props.page?.type == "tag") {
+        query.tagId = props.page.id;
+    }
+    return {
+        key: `taxonomy-${props.page?.title}`,
+        url: "/api/posts",
+        option: {
+            promise: true,
+            fetchOptions: {
+                query
+            },
+        },
+    };
+});
 let postList = computed(() => {
-    return props.page.content;
+    if (props.page?.content?.nodes) {
+        return props.page.content;
+    } else {
+        return { nodes: [] };
+    }
 });
 </script>
 
 <template>
-    <header>
-
-    </header>
-    <PostList :post-list="postList"/>
+    <div class="taxonomy-header">
+        <ContentContainer>
+            <header class="taxonomy-title">
+                <h1>{{ props.page?.title }}</h1>
+                <p>{{ props.page?.description }}</p>
+            </header>
+            <PostList :post-list="postList" :api="api"/>
+        </ContentContainer>
+    </div>
 </template>
 
 <style scoped>
-
+.taxonomy-header {
+    position: relative;
+    overflow: hidden;
+    padding-top: 8rem;
+}
+.taxonomy-title {
+    position: relative;
+    text-align: center;
+    margin-bottom: 6.5%;
+}
 </style>
