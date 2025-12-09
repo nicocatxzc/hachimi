902340820154c3c4b37e360c083fe67672575622
nicocatxzc <nicocatxzc@163.com>
2025-12-09 19:07:15 +0800

    添加页尾

---
+++ app/components/SiteFooter.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+let config = useThemeConfig();
+</script>
+
+<template>
+    <!-- eslint-disable vue/no-v-html -->
+    <footer class="site-footer">
+        <div class="site-info" v-html="config.footerHTML" />
+        <div class="theme-info">
+            <a
+                href="https://github.com/mirai-mamori/Sakurairo"
+                rel="noopener"
+                target="_blank"
+            >
+                Theme Sakurairo
+            </a>
+            <a href="https://docs.fuukei.org/" rel="noopener" target="_blank">
+                by Fuukei
+            </a>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.site-footer {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
