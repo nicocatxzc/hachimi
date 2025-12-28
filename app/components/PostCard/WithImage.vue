b9c228771174ec9c7f08c125c8bdbf67d7e3224d
nicocatxzc <nicocatxzc@163.com>
2025-12-28 17:42:44 +0800

    更新文章卡片布局，防止内容溢出

--- app/components/PostCard/WithImage.vue
+++ app/components/PostCard/WithImage.vue
@@ -12,39 +12,52 @@ let { post } = defineProps({
     <article class="post-card">
         <div class="post-thumb">
             <nuxt-link :to="post.uri">
-                <nuxt-img :src="post.featuredImage?.node.sourceUrl" :alt="`featured image for post ${post.title}`" ></nuxt-img>
+                <nuxt-img
+                    :src="post.featuredImage?.node.sourceUrl"
+                    :alt="`featured image for post ${post.title}`"
+                ></nuxt-img>
             </nuxt-link>
         </div>
 
         <div class="post-date">
-            <time :datetime="post.modifiedGmt">更新于:{{ post.modifiedGmt }}</time>
+            <time :datetime="post.modifiedGmt">
+                更新于:{{ post.modifiedGmt }}
+            </time>
         </div>
 
         <div class="post-meta">
-            <a v-if="post.categories?.nodes[0]" :href="post.categories?.nodes[0].uri"><span>{{ post.categories?.nodes[0].name }}</span></a>
+            <a
+                v-if="post.categories?.nodes[0]"
+                :href="post.categories?.nodes[0].uri"
+            >
+                <span>{{ post.categories?.nodes[0].name }}</span>
+            </a>
         </div>
 
         <div class="post-title">
-          <nuxt-link :to="post.uri" :aria-label="`link to post ${post.title}`">
-            <h3 v-html="post.title"/>
-          </nuxt-link>
+            <nuxt-link
+                :to="post.uri"
+                :aria-label="`link to post ${post.title}`"
+            >
+                <h3 v-html="post.title" />
+            </nuxt-link>
         </div>
         <div class="post-excerpt">
-          <span v-html="post.excerpt" />
+            <span v-html="post.excerpt" />
         </div>
     </article>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .post-card {
-    --meta-background:#33333360;
-    --meta-color:rgba(255,255,255,0.8);
+    --meta-background: #33333360;
+    --meta-color: rgba(255, 255, 255, 0.8);
 }
 * {
     transition: all 0.4s cubic-bezier(0.07, 0.53, 0.65, 0.95);
 }
 .post-card {
-    animation: loading-animation .5s;
+    animation: loading-animation 0.5s;
     width: 100%;
     height: 18.75rem;
     margin: 1rem 0;
@@ -74,7 +87,8 @@ let { post } = defineProps({
 .post-thumb:hover * {
     transform: scale(1.1);
 }
-.post-date,.post-meta {
+.post-date,
+.post-meta {
     position: absolute;
     font-size: 0.75rem;
     white-space: nowrap;
@@ -109,9 +123,9 @@ let { post } = defineProps({
     max-width: 80%;
     padding: 0.63rem 1rem;
     color: var(--word-color-first);
-    background-color: rgba(var(--widget-background),0.7);
+    background-color: rgba(var(--widget-background), 0.7);
     border: 0.01rem solid var(--border-shine);
-    border-radius:0.4rem ;
+    border-radius: 0.4rem;
     backdrop-filter: saturate(180%) blur(0.75px);
 }
 .post-excerpt {
@@ -124,6 +138,12 @@ let { post } = defineProps({
     padding: 0 0.63rem;
     color: var(--word-color-first);
     overflow: hidden;
+    span {
+        display: inline-block;
+        max-height: 3rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
 }
 
 @keyframes loading-animation {
