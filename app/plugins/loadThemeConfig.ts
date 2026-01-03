6676ad068f4d89db633b9b41ac3ee71eec48bebf
nicocatxzc <nicocatxzc@163.com>
2026-01-03 22:44:14 +0800

    添加主题配置初始化提示

--- app/plugins/loadThemeConfig.ts
+++ app/plugins/loadThemeConfig.ts
@@ -1,5 +1,17 @@
-export default defineNuxtPlugin(async () => {
+export default defineNuxtPlugin(async (nuxtApp) => {
     const themeConfig = useThemeConfigStore();
-    const {data} = await useCachedFetch("themeConfig","/api/theme/themeConfig")
-    themeConfig.config = data.value as object
+    const { data } = await useCachedFetch(
+        "themeConfig",
+        "/api/theme/themeConfig"
+    );
+    themeConfig.config = data.value as object;
+
+    if (import.meta.client) {
+        if (!data.value?.activeColor ?? "") {
+            ElMessage.primary("未检测到主题配置，请登录并初始化设置！");
+            document.addEventListener("pjax:complete",()=>{
+                navigateTo("/dashboard")
+            },{once:true})
+        }
+    }
 });
